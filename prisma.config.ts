40bc58ce4e6d1e0cc31d3af6a670afd526792f16
Kirat Goel <kirat24303@iiitd.ac.in>
2026-05-14 16:24:40 +0530

    Fix prisma config for Render

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,13 +1,8 @@
-import "dotenv/config";
-import { defineConfig, env } from "prisma/config";
+import { defineConfig } from "prisma/config";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 export default defineConfig({
-  schema: "prisma/schema.prisma",
-  migrations: {
-    path: "prisma/migrations",
-  },
-  datasource: {
-    // This replaces `url = env("DATABASE_URL")` from schema.prisma
-    url: env("DATABASE_URL"),
-  },
+    schema: "prisma/schema.prisma",
 });
